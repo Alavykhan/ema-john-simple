adb3f2c2eb114d95d3c351cdebfa882aca5ab98f
alavy <alavykhan@gmail.com>
2023-03-31 12:01:10 +0600

    new feature added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart} from '@fortawesome/free-solid-svg-icons'
+
 const Product = (props) => {
     const {img, name, seller, rating, price } = props.product;
+    const handleAddToCart = props.handleAddToCart;
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -11,7 +15,9 @@ const Product = (props) => {
             <p>Manufacturer: {seller}</p>
             <p>Rating: {rating}</p>
             </div>
-            <button className='addToCart'>Add to Cart</button>
+            <button onClick={()=>handleAddToCart(props.product)} className='addToCart'>Add to Cart
+             <FontAwesomeIcon icon={faShoppingCart} />
+            </button>
         </div>
     );
 };
